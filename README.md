2ab89fbf2804bb4be3cb2d0f74611b972eaab3a7
Brianca Knight <briancaknight@gmail.com>
2023-10-31 10:48:56 -0700

    Add test 2 create order message.
    Co-authored-by Christopher Quattrocchi <chrisquattrocchi@gmail.com> and Trent Dietzel <dietzeltrent@gmail.com>

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 Describe Ticket()
 
+SUCCESS
 Test: It should create a constructor for Tickets.
 Code: Tickets(movie1, movie2)
 Expected Output: movie1, movie2
 
-Test:
-Code:
-Expected Output:
+Test: It should add a ticket order message.
+Code: Ticket.orderMessage()
+Expected Output: "You have bought a "age" ticket to the "time" showing of movie ("release").
 
 Test:
 Code:
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-function Ticket(releaseType, showing, age){
+function Ticket(releaseType, showing, age, movie){
   this.releaseType = releaseType;
   this.showing = showing;
-  this.age = age;
+  this.age = age
+  this.movie = movie;
 }
 
+Ticket.prototype.orderMessage = function() {
+  return "You have bought a " + this.age + " ticket to the " + this.showing + " showing of " + this.movie + " (" + this.releaseType + ")"
+}
